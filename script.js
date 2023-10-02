8b87b3ba55a245da7cabeaae02a964cb8d7183a7
shailja <145833769+shailja125@users.noreply.github.com>
2023-10-02 17:11:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function convertToFar() {
+  var celsius = document.getElementById("celsius").value;
+  var fahrenheit = (celsius * 9/5) + 32;
+  document.getElementById("fahrenheit").value = fahrenheit;
+}
